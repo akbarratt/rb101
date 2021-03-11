c2c3293e8221781fb862936d04f56691281ba3c0
AK Barratt <andria@akbarratt.com>
2021-03-11 14:35:54 -0500

    Refactor code.

--- lesson_2/calculator_bonus/calculator.rb
+++ lesson_2/calculator_bonus/calculator.rb
@@ -20,11 +20,11 @@ def get_language
   end
 end
 
-def get_name
+def get_name(lang)
   loop do
     input = gets.chomp
     return input unless input.empty?
-    prompt(MESSAGES[LANGUAGE]["valid_name"])
+    prompt(MESSAGES[lang]["valid_name"])
   end
 end
 
@@ -40,13 +40,13 @@ def valid_number?(input)
   integer?(input) || float?(input)
 end
 
-def get_number
+def get_number(lang)
   loop do
     input = gets.chomp
     if valid_number?(input)
       return input.to_f
     else
-      prompt(MESSAGES[LANGUAGE]["invalid_number"])
+      prompt(MESSAGES[lang]["invalid_number"])
     end
   end
 end
@@ -54,35 +54,31 @@ end
 system("clear")
 
 prompt(MESSAGES["language"])
-LANGUAGE = get_language
+language = get_language
 
 # Extrapolate into get name method
-prompt(MESSAGES[LANGUAGE]["name_prompt"])
-name = get_name
+prompt(MESSAGES[language]["name_prompt"])
+name = get_name(language)
 
-prompt(MESSAGES[LANGUAGE]["welcome"] + name + ".")
+prompt(MESSAGES[language]["welcome"] + name + ".")
 
 loop do # main loop
-  prompt(MESSAGES[LANGUAGE]["input1"])
-  number1 = get_number
-  prompt(MESSAGES[LANGUAGE]["input2"])
-  number2 = get_number
+  prompt(MESSAGES[language]["input1"])
+  number1 = get_number(language)
+  prompt(MESSAGES[language]["input2"])
+  number2 = get_number(language)
 
-  prompt(MESSAGES[LANGUAGE]["operator"])
+  prompt(MESSAGES[language]["operator"])
 
   # Extrapolate into get operation method
   operator = ''
   loop do
     operator = gets.chomp
     break if %w(1 2 3 4).include?(operator)
-    prompt(MESSAGES[LANGUAGE]["invalid_operator"])
+    prompt(MESSAGES[language]["invalid_operator"])
   end
 
-  prompt((MESSAGES[LANGUAGE][operator]) + (MESSAGES[LANGUAGE]["numbers"]))
-
-  # Conversion of user input to float. (This could be a method?)
-  number1 = number1.to_f
-  number2 = number2.to_f
+  prompt((MESSAGES[language][operator]) + (MESSAGES[language]["numbers"]))
 
   # Extrapolate into perform operation method
   result = case operator
@@ -98,21 +94,23 @@ loop do # main loop
 
   # Print result as integer or float, as appropriate. Extrapolate into display result method
   if result % 1 == 0.0
-    prompt(MESSAGES[LANGUAGE]["result"] + result.to_i.to_s)
+    prompt(MESSAGES[language]["result"] + result.to_i.to_s)
   else
-    prompt(MESSAGES[LANGUAGE]["result"] + result.to_s)
+    prompt(MESSAGES[language]["result"] + result.to_s)
   end
 
   # Extrapolate into calculate again? method.
-  prompt(MESSAGES[LANGUAGE]["repeat"])
+  prompt(MESSAGES[language]["repeat"])
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt(name + MESSAGES[LANGUAGE]["goodbye"])
+prompt(name + MESSAGES[language]["goodbye"])
 
 # To Do:
 # Clear screen between calculations.
 # Disallow divide by 0.
 # Disallow input other than supported languages.
 # Disallow input other than y, yes, n, or no. (Other languages?)
+
+# To get around the scoping problem of LANGUAGES, other than by making it a constant (possible not best practice?) I would have to have the methods take a parameter of the language.
