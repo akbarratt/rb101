31ddd134d5d6d389e16ebff35bb5787f3e62d582
AK Barratt <andria@akbarratt.com>
2021-03-11 14:08:06 -0500

    Create and incorporate get_language and get_name methods.

--- lesson_2/calculator_bonus/calculator.rb
+++ lesson_2/calculator_bonus/calculator.rb
@@ -6,6 +6,28 @@ def prompt(message)
   puts("=> #{message}")
 end
 
+def get_language
+  loop do
+    input = gets.chomp
+    if input.empty?
+      prompt(MESSAGES["language_error"])
+    elsif SUPPORTED_LANGUAGES.include?(input)
+      return input
+    else
+      prompt(MESSAGES["language_error"])
+    end
+    # Bug: I have to explicitly check for empty input or it returns an empty string. Running into explicit return.
+  end
+end
+
+def get_name
+  loop do
+    input = gets.chomp
+    return input unless input.empty?
+    prompt(MESSAGES[LANGUAGE]["valid_name"])
+  end
+end
+
 def integer?(num)
   num.to_i.to_s == num
 end
@@ -18,54 +40,52 @@ def valid_number?(input)
   integer?(input) || float?(input)
 end
 
+system("clear")
+
 prompt(MESSAGES["language"])
-language = gets.chomp
-unless SUPPORTED_LANGUAGES.include?(language)
-  language = "en"
-end
+LANGUAGE = get_language
 
-prompt(MESSAGES[language]["name_prompt"])
-name = ''
-loop do
-  name = gets.chomp
-  break unless name.empty?
-  prompt(MESSAGES[language]["valid_name"])
-end
+# Extrapolate into get name method
+prompt(MESSAGES[LANGUAGE]["name_prompt"])
+name = get_name
 
-prompt(MESSAGES[language]["welcome"] + name + ".")
+prompt(MESSAGES[LANGUAGE]["welcome"] + name + ".")
 
+# Redundant, so extrapolate into a get number method
 loop do # main loop
   number1 = ''
   loop do
-    prompt(MESSAGES[language]["input1"])
+    prompt(MESSAGES[LANGUAGE]["input1"])
     number1 = gets.chomp
     break if valid_number?(number1)
-    prompt(MESSAGES[language]["invalid_number"])
+    prompt(MESSAGES[LANGUAGE]["invalid_number"])
   end
 
   number2 = ''
   loop do
-    prompt(MESSAGES[language]["input2"])
+    prompt(MESSAGES[LANGUAGE]["input2"])
     number2 = gets.chomp
     break if valid_number?(number2)
-    prompt(MESSAGES[language]["invalid_number"])
+    prompt(MESSAGES[LANGUAGE]["invalid_number"])
   end
 
-  prompt(MESSAGES[language]["operator"])
+  prompt(MESSAGES[LANGUAGE]["operator"])
 
+  # Extrapolate into get operation method
   operator = ''
   loop do
     operator = gets.chomp
     break if %w(1 2 3 4).include?(operator)
-    prompt(MESSAGES[language]["invalid_operator"])
+    prompt(MESSAGES[LANGUAGE]["invalid_operator"])
   end
 
-  prompt((MESSAGES[language][operator]) + (MESSAGES[language]["numbers"]))
+  prompt((MESSAGES[LANGUAGE][operator]) + (MESSAGES[LANGUAGE]["numbers"]))
 
-  # Conversion of user input to float.
+  # Conversion of user input to float. (This could be a method?)
   number1 = number1.to_f
   number2 = number2.to_f
 
+  # Extrapolate into perform operation method
   result = case operator
            when "1"
              number1 + number2
@@ -77,16 +97,23 @@ loop do # main loop
              number1 / number2
            end
 
-  # Print result as integer or float, as appropriate.
+  # Print result as integer or float, as appropriate. Extrapolate into display result method
   if result % 1 == 0.0
-    prompt(MESSAGES[language]["result"] + result.to_i.to_s)
+    prompt(MESSAGES[LANGUAGE]["result"] + result.to_i.to_s)
   else
-    prompt(MESSAGES[language]["result"] + result.to_s)
+    prompt(MESSAGES[LANGUAGE]["result"] + result.to_s)
   end
 
-  prompt(MESSAGES[language]["repeat"])
+  # Extrapolate into calculate again? method.
+  prompt(MESSAGES[LANGUAGE]["repeat"])
   answer = gets.chomp
   break unless answer.downcase.start_with?('y')
 end
 
-prompt(name + MESSAGES[language]["goodbye"])
+prompt(name + MESSAGES[LANGUAGE]["goodbye"])
+
+# To Do:
+# Clear screen between calculations.
+# Disallow divide by 0.
+# Disallow input other than supported languages.
+# Disallow input other than y, yes, n, or no. (Other languages?)
