194f580315f5b38a6853e4a48d379bcdd5d097cd
akbarratt <andria@akbarratt.com>
2021-03-19 14:53:01 -0500

    Begin exercise 2.

---
+++ exercises/easy1/02.rb
@@ -0,0 +1,20 @@
+# def is_odd?(int)
+#   if int == 0
+#     return false
+#   elsif (int%2 == 0)
+#     return false
+#   else
+#     return true
+#   end
+# end
+
+def is_odd?(int)
+  int % 2 == 1
+end
+
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
