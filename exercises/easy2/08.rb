52355357e84e7f622338903cfbc2488453b144e1
AK Barratt <andria@akbarratt.com>
2021-04-10 23:22:18 -0400

    Continue work on exercise 8

--- exercises/easy2/08.rb
+++ exercises/easy2/08.rb
@@ -1,14 +1,15 @@
-def create_array(integer)
-  array = 
-end
-
-def parse_operator(string, range)
- if string == 's'
-  for x in range
- elsif string == 'm'
- else
-  puts "Error! Invalid operator."
- end
+def calculate (operator, int)
+  if operator == 's'
+    sum = 0
+    1.upto(int) { |num| sum += num}
+    sum
+  elsif operator == 'p'
+    product = 1
+    1.upto(int) { |num| product *= num }
+    product
+  else
+    puts 'Error!'
+  end
 end
 
 puts ">> Please enter an integer greater than 0:"
@@ -16,4 +17,8 @@ number = gets.chomp.to_i
 puts ">> Enter 's' to compute the sum, 'p' to compute the product."
 operator = gets.chomp
 
-puts "The sum of the integers between 1 and 5 is 15."
+calculate(operator, number)
+
+puts "The sum of the integers between 1 and #{number} is #{calculate(operator, number)}."
+
+# Need to do Further Exploration
