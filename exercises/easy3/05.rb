6e68f5ce09c07c4a8c3e73103c1435d5baa8b614
AK Barratt <andria@akbarratt.com>
2021-04-21 16:35:23 -0400

    Continue Easy 3 exercises

--- exercises/easy3/05.rb
+++ exercises/easy3/05.rb
@@ -6,8 +6,8 @@ def square(num)
   multiply(num, num)
 end
 
-p square(5) == 25
-p square(-8) == 64
-
 # Further Exploration: What if we wanted to generalize this method to a "power to the n" type method: cubed, to the 4th power, to the 5th, etc. How would we go about doing so while still using the multiply method?
 
+def power(num, power)
+  multiply(num, 1)**power
+end
