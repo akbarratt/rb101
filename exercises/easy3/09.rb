ba499a3e46783dfaa9e857f4f9e76dd71ff02b51
AK Barratt <andria@akbarratt.com>
2021-04-21 23:17:23 -0400

    Complete Easy 3 exercises

--- exercises/easy3/09.rb
+++ exercises/easy3/09.rb
@@ -1,8 +1,17 @@
 ALPHANUMERICS = [("a".."z").to_a, ("A".."Z").to_a, (0..9).to_a].flatten
 
-real_palindrome?('madam') == true
-real_palindrome?('Madam') == true           # (case does not matter)
-real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
-real_palindrome?('356653') == true
-real_palindrome?('356a653') == true
-real_palindrome?('123ab321') == false
+def transform(string)
+  array = string.chars.delete_if { |char| !ALPHANUMERICS.include?(char)}
+  array.join.downcase
+end
+
+def real_palindrome?(string)
+  transform(string) == transform(string).reverse
+end
+
+p real_palindrome?('madam') == true
+p real_palindrome?('Madam') == true           # (case does not matter)
+p real_palindrome?("Madam, I'm Adam") == true # (only alphanumerics matter)
+p real_palindrome?('356653') == true
+p real_palindrome?('356a653') == true
+p real_palindrome?('123ab321') == false
