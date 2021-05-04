bdf0f1e6126592f0f0bf17379b41a33675f5245c
AK Barratt <andria@akbarratt.com>
2021-05-03 23:10:35 -0400

    Continue work on exercise 6

--- exercises/easy4/06.rb
+++ exercises/easy4/06.rb
@@ -10,15 +10,26 @@
 #   result
 # end
 
+# Further Exploration
+
+# def running_total(array)
+#   sum = 0
+#   array.each_with_object([]) do |item, ary|
+#     ary << item + sum
+#     sum = item + sum
+#   end
+# end
+
 def running_total(array)
-  sum = 0
-  array.each_with_object([]) do |item, ary|
-    ary << item + sum
-    sum = item + sum
+  # sum = 0
+  array.inject([]) do |result, item|
+    result << result += item
   end
 end
 
-p running_total([2, 5, 13]) == [2, 7, 20]
-p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
-p running_total([3]) == [3]
-p running_total([]) == []
+p running_total([2, 5, 13])
+
+# p running_total([2, 5, 13]) == [2, 7, 20]
+# p running_total([14, 11, 7, 15, 20]) == [14, 25, 32, 47, 67]
+# p running_total([3]) == [3]
+# p running_total([]) == []
