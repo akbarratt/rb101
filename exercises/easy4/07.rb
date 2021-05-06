0508a8e1615c1deef5c68a1b0e4f4648ffcada7a
AK Barratt <andria@akbarratt.com>
2021-05-06 12:19:07 -0400

    Continue Easy 4 exercises.

--- exercises/easy4/07.rb
+++ exercises/easy4/07.rb
@@ -1,5 +1,15 @@
+NUMBERS = ('0'..'9').zip(0..9).to_h
+
 def string_to_integer(string)
-  string.chars.join.to_i
+  digits = string.chars.map { |char| NUMBERS[char] }
+  digits.reverse!
+  result = 0
+  tens = 1
+  digits.each do |num|
+    result += num * tens
+    tens *= 10
+  end
+  result
 end
 
 p string_to_integer('4321') == 4321
@@ -12,11 +22,21 @@ HEX = {
   '5' => 5, '6' => 6, '7' => 7, '8' => 8, '9' => 9, 'A' => 10, 'B' => 11, 'C' => 12, 'D' => 13, 'E' => 14, 'F' => 15
 }
 
-# 0b prefix in Ruby indicates a binary literal.
-
 def hexadecimal_to_integer(string)
-
+  digits = string.upcase.chars.map { |char| HEX[char] }
+  digits.reverse!
+  result = 0
+  hex_place = 1
+  digits.each do |num|
+    result += num * hex_place
+    hex_place *= 16
+  end
+  result
 end
 
+p hexadecimal_to_integer('4D9f') == 19871
+
+
+# 0b prefix in Ruby indicates a binary literal.
 # https://www.honeybadger.io/blog/bitwise-hacks-in-ruby/
 # https://www.youtube.com/watch?v=t_kA5KQxByc
