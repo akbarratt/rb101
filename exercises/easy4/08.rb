7ff815295ef1683c2686b58e2a938cef017cb9dd
akbarratt <andria@akbarratt.com>
2021-05-07 16:02:41 -0400

    Continue work on exercise 8

--- exercises/easy4/08.rb
+++ exercises/easy4/08.rb
@@ -2,13 +2,28 @@ require 'pry'
 
 NUMBERS = ('0'..'9').zip(0..9).to_h
 
-def string_to_signed_integer(string)
-  if string.first == '-' || digits.first == '+'
-    sign = digits.shift
-  else
-    sign = '+'
-  end
-  digits = string.chars.map { |char| char = NUMBERS[char] }
+# def string_to_signed_integer(string)
+#   if string.first == '-' || digits.first == '+'
+#     sign = digits.shift
+#   else
+#     sign = '+'
+#   end
+#   digits = string.chars.map { |char| char = NUMBERS[char] }
+#   digits.reverse!
+#   result = 0
+#   tens = 1
+#   digits.each do |num|
+#     result += num * tens
+#     tens *= 10
+#   end
+#   return -result if sign == '-'
+#   result
+# end
+
+# Further Exploration
+
+def string_to_integer(string)
+  digits = string.chars.map { |char| NUMBERS[char] }
   digits.reverse!
   result = 0
   tens = 1
@@ -16,8 +31,18 @@ def string_to_signed_integer(string)
     result += num * tens
     tens *= 10
   end
-  return -result if sign == '-'
   result
+end
+
+def string_to_signed_integer(string)
+  digits = string[1..-1] if NUMBERS.include?(string[0]) == false
+
+
+  case string[0]
+  when '-' then -string_to_integer(string[1..-1])
+  when '+' then string_to_integer(string[1..-1])
+  else          string_to_integer(string)
+  end
 end
 
 p string_to_signed_integer('4321') == 4321
