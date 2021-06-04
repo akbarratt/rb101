bee3ab82a9a12f1b1195fc811e0709f49b7d31b7
AK Barratt <andria@akbarratt.com>
2021-06-04 15:54:51 -0400

    Complete exercise 2

--- exercises/easy5/02.rb
+++ exercises/easy5/02.rb
@@ -1,11 +1,24 @@
 def time_of_day(int)
-  if num >= 0
-    num -= 1440 until num <= 1439
+  if int >= 0
+    int -= 1440 until int <= 1439
   else
-    num += 1440 until num >= -1439
+    int += 1440 until int >= -1439
+    int += 1440
   end
-  return num
+  minutes = (int % 60).to_s
+  hours = (int / 60).to_s
+  hours.prepend("0") if hours.size == 1
+  minutes.prepend("0") if minutes.size == 1
+  return "#{hours}:#{minutes}"
 end
+
+p time_of_day(0) == "00:00"
+p time_of_day(-3) == "23:57"
+p time_of_day(35) == "00:35"
+p time_of_day(-1437) == "00:03"
+p time_of_day(3000) == "02:00"
+p time_of_day(800) == "13:20"
+p time_of_day(-4231) == "01:29"
 
 =begin
 # Problem
@@ -48,5 +61,12 @@ END
 
 num % 60 will give the number of minutes
 num / 60.floor will give the number of hours.
+
+
+Notes on solution:
+ - I did addition and subtraction rather than multiplication and division, which would have been more straightforward.
+ - Also neglected to use CONSTS in my solution. As we know, any number whose significance is not immediately known should be saved into a variable or const (labeled).
+ - Thought about using multiple methods in my solution but decided against it. That would have been the right call. Everything should be abstracted down to its smallest piece.
+ - I still don't quite understand how Kernel#format works.
 
 =end
