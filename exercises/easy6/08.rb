005128a0aa61496d0e1510b9ba3be3ac94ef9f5f
AK Barratt <andria@akbarratt.com>
2021-06-11 19:35:22 -0400

    Complete exercise 6

---
+++ exercises/easy6/08.rb
@@ -0,0 +1,56 @@
+=begin
+# Problem
+Given an array of integers, write a method that will find and return the one duplicate value known to be in the array. Assume there is only one duplicate value.
+
+Essentially we are making a form of Array#uniq, however uniq doens't actually return the duplicate objects but a new array with the duplicate objects removed. Essentially, knowing there is only one duplicate aross the array, we're looking for the first duplicated value.
+
+# Input: Array
+# Output: Integer
+
+# Examples
+find_dup([1, 5, 3, 1]) == 1
+find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+          38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+          14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+          78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+          89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+          41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+          55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+          85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+          40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+          7,  34, 57, 74, 45, 11, 88, 67,  5, 58]) == 73
+
+# Data Structures
+Array
+
+# Algorithm
+Given an array
+Loop through each item in the array.
+For each iteration, assign the current object to the variable current_num
+Compare current num to see if the new array contains the current number
+If not, add the current number to the new array and continue
+If so, return the current_num integer.
+
+=end
+
+def find_dup(array)
+  new_array = []
+  counter = 0
+  until counter == array.length || new_array.include?(array[counter])
+    new_array << array[counter]
+    counter += 1
+  end
+  array[counter]
+end
+
+p find_dup([1, 5, 3, 1]) == 1
+p find_dup([18,  9, 36, 96, 31, 19, 54, 75, 42, 15,
+          38, 25, 97, 92, 46, 69, 91, 59, 53, 27,
+          14, 61, 90, 81,  8, 63, 95, 99, 30, 65,
+          78, 76, 48, 16, 93, 77, 52, 49, 37, 29,
+          89, 10, 84,  1, 47, 68, 12, 33, 86, 60,
+          41, 44, 83, 35, 94, 73, 98,  3, 64, 82,
+          55, 79, 80, 21, 39, 72, 13, 50,  6, 70,
+          85, 87, 51, 17, 66, 20, 28, 26,  2, 22,
+          40, 23, 71, 62, 73, 32, 43, 24,  4, 56,
+          7,  34, 57, 74, 45, 11, 88, 67,  5, 58]) == 73
