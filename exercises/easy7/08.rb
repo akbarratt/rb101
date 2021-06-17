57b12d82b8819465fd26d539e6e58f5a7a0179d5
AK Barratt <andria@akbarratt.com>
2021-06-17 15:40:27 -0400

    Begin exercise 8

---
+++ exercises/easy7/08.rb
@@ -0,0 +1,15 @@
+=begin
+Given 2 arrays of equal size, return an array consisting of the product of each pair of arrays of the same index number from each given array.
+
+Input: 2 arrays
+Output: 1 array
+
+Example:
+multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+
+# Data Structures
+Array
+
+# Algorithm
+
+=end
