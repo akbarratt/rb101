0b7ad8df25164017027588ad8ae90576850a3dfc
AK Barratt <andria@akbarratt.com>
2021-06-17 15:54:11 -0400

    Complete exercise 8

--- exercises/easy7/08.rb
+++ exercises/easy7/08.rb
@@ -1,3 +1,15 @@
+def multiply_list(arr1, arr2)
+  index = 0
+  result = []
+  until index == arr1.size
+    result << arr1[index] * arr2[index]
+    index += 1
+  end
+  result
+end
+
+p multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
+
 =begin
 Given 2 arrays of equal size, return an array consisting of the product of each pair of arrays of the same index number from each given array.
 
@@ -11,5 +23,11 @@ multiply_list([3, 5, 7], [9, 10, 11]) == [27, 50, 77]
 Array
 
 # Algorithm
-
+Given 2 arrays of equal length
+Set a counter to 0
+Set a result variable to an empty array
+Until counter == result.length
+  result << arr[counter] * arr2[counter]
+  counter +=1
+Return result.
 =end
