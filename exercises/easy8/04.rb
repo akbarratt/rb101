d7b0711a0205782c3bf79d4a22825b2ea868705c
akbarratt <andria@akbarratt.com>
2021-06-18 16:01:33 -0400

    Continue work on exercise 4

--- exercises/easy8/04.rb
+++ exercises/easy8/04.rb
@@ -3,15 +3,17 @@ def leading_substrings(string)
   1.upto(string.size) do |num|
     results << string.chars.combination(num).to_a
   end
-  p results.map do |subarray|
+  results.map do |subarray|
     subarray.map do |substring|
       substring.flatten
+      # Why doesn't this flatten the first array of single character strings?
     end
+    subarray
   end
   # Not sure why this is returning an enumerator.
 end
 
-leading_substrings('abcde')
+p leading_substrings('abcde')
 
 =begin
 # Problem
