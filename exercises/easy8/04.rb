0470c98419d4d33a8042134ede1b8988f91440a2
AK Barratt <andria@akbarratt.com>
2021-06-19 22:22:07 -0400

    Solve the wrong problem

--- exercises/easy8/04.rb
+++ exercises/easy8/04.rb
@@ -4,21 +4,11 @@ def leading_substrings(string)
     results << string.chars.combination(num).to_a
   end
   results.map do |subarray|
-    subarray.map do |substring|
-      substring.flatten
-      # Why doesn't this flatten the first array of single character strings?
+    subarray.map! do |substring|
+      substring.join
     end
-    subarray
   end
-  # Not sure why this is returning an enumerator.
+  results.flatten.sort
 end
 
 p leading_substrings('abcde')
-
-=begin
-# Problem
-We could use Array#combination for Integer#upto(array.size) times with an incrementing index number.
-
-
-
-=end
