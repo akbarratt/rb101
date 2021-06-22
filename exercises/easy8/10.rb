55f779f88d5b4d712d9c25aeda09ac4691a24666
AK Barratt <andria@akbarratt.com>
2021-06-22 13:50:08 -0400

    Complete exercise 10

---
+++ exercises/easy8/10.rb
@@ -0,0 +1,26 @@
+def center_of(string)
+  middle_index = string.size/2
+  if string.chars.size.odd?
+    string.chars[middle_index]
+  else
+    string.chars[middle_index-1..middle_index].join
+  end
+end
+
+p center_of('I love ruby') == 'e'
+p center_of('Launch School') == ' '
+p center_of('Launch') == 'un'
+p center_of('Launchschool') == 'hs'
+p center_of('x') == 'x'
+
+=begin
+Given a string, return the middle 1 or 2 characters from the string.
+
+First, break the string down into an array of its characters
+  If the size is odd
+    return the character in the string that has the index of length/2
+  If the size is even
+    return the character that has the index of length/2 and the next character
+
+
+=end
