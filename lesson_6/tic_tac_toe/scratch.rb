c7da67b6111bb28454ced4352f70e781503534ed
AK Barratt <andria@akbarratt.com>
2021-07-03 19:34:10 -0400

    Complete coin_toss

--- lesson_6/tic_tac_toe/scratch.rb
+++ lesson_6/tic_tac_toe/scratch.rb
@@ -1,4 +1,4 @@
-COIN = ['heads', 'tails']
+COIN = ['HEADS', 'TAILS']
 
 def prompt(msg)
   puts "=> #{msg}"
@@ -16,7 +16,25 @@ end
 
 def coin_toss
   prompt 'We will toss a coin to determine who plays first.'
-  prompt "Choose a side, #{joinor(COIN)}"
+  prompt "Choose a side, #{joinor(COIN)}."
+  user_coin = gets.chomp.upcase
+  unless COIN.include?(user_coin)
+    prompt 'Invalid response. Assigning a side...'
+    sleep(1)
+    user_coin = COIN.sample
+  end
+  prompt "You have chosen #{user_coin}."
+  prompt "Tossing coin..."
+  sleep(1)
+  coin_results = COIN.sample
+  prompt "It's #{coin_results}!"
+  if user_coin == coin_results
+    prompt "You go first, #PLAYERNAME." # placeholder
+    # set player to have first turn
+  else
+    prompt "The computer goes first."
+    #set computer to have first turn
+  end
 end
 
 coin_toss
