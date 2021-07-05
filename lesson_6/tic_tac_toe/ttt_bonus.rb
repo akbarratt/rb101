d0f2109b653dba0c5af89a65dce30aad73426d0b
AK Barratt <andria@akbarratt.com>
2021-07-05 18:07:06 -0400

    Refactor

--- lesson_6/tic_tac_toe/ttt_bonus.rb
+++ lesson_6/tic_tac_toe/ttt_bonus.rb
@@ -4,8 +4,6 @@ WINNING_LINES = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] + # rows
                 [[1, 4, 7], [2, 5, 8], [3, 6, 9]] + # columns
                 [[1, 5, 9], [3, 5, 7]] # diagonals
 INITIAL_MARKER = ' '
-PLAYER_MARKER = 'X'
-COMPUTER_MARKER = 'O'
 COIN = ['HEADS', 'TAILS']
 
 def prompt(msg)
@@ -176,6 +174,8 @@ com = {
   token: '',
   wins: 0,
 }
+
+current_player = 
 
 initialize_game(user, com)
 game_loop(user, com)
