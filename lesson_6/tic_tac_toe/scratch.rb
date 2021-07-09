f1b8504b5455dbaf9e2a905d6c8ddfe7be4569ed
AK Barratt <andria@akbarratt.com>
2021-07-08 22:00:28 -0400

    Messing everything up

--- lesson_6/tic_tac_toe/scratch.rb
+++ lesson_6/tic_tac_toe/scratch.rb
@@ -1,6 +1,14 @@
-line = {1 => 'X', 2 => 'X', 3 => ' '}
+brd = {1 => 'X', 2 => 'X', 3 => ' ', 4=> ' ', 5=> ' ', 6 => ' ', 7 => ' ', 8 => ' ', 9=> ' '}
+line = [1, 2, 3]
 
-def square_threatened?(line)
-  return true if line.values_at(1, 2, 3).count('X') == THREATENED_MARKERS
-  false
+def square_threatened?(brd, line)
+  !!defend_square(brd, line)
 end
+
+def defend_square(brd, line)
+  return brd.key(' ') if brd.values_at(1, 2, 3).count('X') == 2
+  nil
+end
+
+p square_threatened?(brd, line)
+p defend_square(brd, line)
