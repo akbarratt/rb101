d1f14eac87cf9a37ed7c76762e3bedd0ea61896f
AK Barratt <andria@akbarratt.com>
2021-07-20 16:13:00 -0400

    Rearrange methods to better align with logic

--- lesson_6/twenty_one/twenty_one_bonus.rb
+++ lesson_6/twenty_one/twenty_one_bonus.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require 'pry-byebug'
 
 SUITS = ['S', 'H', 'C', 'D']
 VALUES = ['A', 'K', 'Q', 'J', '10', '9', '8', '7', '6', '5', '4', '3', '2']
@@ -9,6 +10,32 @@ def prompt(msg)
   puts "=> #{msg}"
 end
 
+def play_game(player, dealer)
+  loop do
+    deck = generate_deck(SUITS, VALUES)
+    prompt "Dealing..."
+    sleep(1)
+    player[:hand] = deal_cards(deck, 2)
+    player[:total] = hand_value(player[:hand])
+    dealer_hand = deal_cards(deck, 2)
+    player_turn(player, dealer_hand, deck)
+    if bust?(hand_value(player_hand))
+      prompt "You busted!"
+    else
+      prompt "Dealer is thinking..."
+      sleep(1)
+      dealer_turn(dealer_hand, deck)
+      if bust?(hand_value(dealer_hand))
+        prompt "Dealer busted!"
+      end
+    end
+    game_over(player_hand, dealer_hand, false, true)
+    prompt "Play again?"
+    answer = gets.chomp
+    break if answer == 'no'
+  end
+end
+
 def generate_deck(suits, values)
   values.product(suits).map(&:join).shuffle
 end
@@ -17,41 +44,10 @@ def deal_cards(deck, num_cards)
   deck.pop(num_cards)
 end
 
-def hit(deck)
-  deal_cards(deck, 1) unless deck.empty?
-end
-
-def hand_value(hand)
-  total = 0
-  hand.each do |card|
-    case card[0]
-    when 'A'
-      total += 11
-    when 'K', 'Q', 'J', '1' # '1' is for 10, the only possible 3-digit string
-      total += 10
-    else
-      total += card[0].to_i
-    end
-  end
-
-  if bust?(total)
-    aces = hand.select { |card| card[0] == 'A' }
-    until aces.empty? || !bust?(total)
-      total -= 10
-      aces.pop
-    end
-  end
-  total
-end
-
-def bust?(num)
-  num > WINNING_LIMIT
-end
-
-def player_turn(player_hand, dealer_hand, deck)
+def player_turn(player, dealer_hand, deck)
   loop do
-    game_status(player_hand, dealer_hand, true)
-    break if hand_value(player_hand) == WINNING_LIMIT || bust?(hand_value(player_hand))
+    game_status(player, dealer_hand, true)
+    break if player[:hand] == WINNING_LIMIT || bust?(player[:total])
     answer = player_choice
     if answer == 'hit'
       prompt "Drawing a card..."
@@ -66,13 +62,55 @@ def player_turn(player_hand, dealer_hand, deck)
   end
 end
 
-def game_status(player_hand, dealer_hand, obscure=false, dealer_point=false)
+def game_status(player, dealer_hand, obscure=false, dealer_point=false)
   prompt "**********"
   prompt "Dealer hand: #{display_hand(dealer_hand, obscure)}"
   prompt "Dealer points: #{hand_value(dealer_hand)}" if dealer_point == true
-  prompt "Your hand: #{display_hand(player_hand)}"
-  prompt "Your points: #{hand_value(player_hand)}"
+  # binding.pry
+  prompt "Your hand: #{display_hand(player[:hand])}"
+  prompt "Your points: #{hand_value(player[:total])}"
   prompt "**********"
+end
+
+def display_hand(hand, obscure=false)
+  display = ''
+  hand.each do |card|
+    if card == hand [0] && obscure == true
+      display << '??' + ' '
+    elsif card == hand[-1]
+      display << card
+    else
+      display << card + ' '
+    end
+  end
+  display
+end
+
+def hand_value(player)
+  # Refactor using hash
+  player[:hand].each do |card|
+    case card[0]
+    when 'A'
+      player[:total] += 11
+    when 'K', 'Q', 'J', '1' # '1' is for 10, the only possible 3-digit string
+      player[:total] += 10
+    else
+      player[:total] += card[0].to_i
+    end
+  end
+
+  if bust?(player[:total])
+    aces = player[:hand].select { |card| card[0] == 'A' }
+    until aces.empty? || !bust?(player[:total])
+      player[:total] -= 10
+      aces.pop
+    end
+  end
+  player[:total]
+end
+
+def bust?(num)
+  num > WINNING_LIMIT
 end
 
 def player_choice
@@ -86,20 +124,13 @@ def player_choice
   answer
 end
 
+def hit(deck)
+  deal_cards(deck, 1) unless deck.empty?
+end
+
 def dealer_turn(hand, deck)
   until hand_value(hand) >= DEALER_STAY || bust?(hand_value(hand))
     hand.concat(hit(deck))
-  end
-end
-
-def game_over(player_hand, dealer_hand, obscure, dealer_point)
-  game_status(player_hand, dealer_hand, obscure, dealer_point)
-  if determine_winner(player_hand, dealer_hand) == :dealer_win
-    prompt "Dealer wins!"
-  elsif determine_winner(player_hand, dealer_hand) == :player_win
-    prompt "You win!"
-  elsif determine_winner(player_hand, dealer_hand) == :tie
-    prompt "It's a tie!"
   end
 end
 
@@ -117,42 +148,14 @@ def determine_winner(player_hand, dealer_hand)
   end
 end
 
-def display_hand(hand, obscure=false)
-  display = ''
-  hand.each do |card|
-    if card == hand [0] && obscure == true
-      display << '??' + ' '
-    elsif card == hand[-1]
-      display << card
-    else
-      display << card + ' '
-    end
-  end
-  display
-end
-
-def play_game
-  loop do
-    deck = generate_deck(SUITS, VALUES)
-    prompt "Dealing..."
-    sleep(1)
-    player_hand = deal_cards(deck, 2)
-    dealer_hand = deal_cards(deck, 2)
-    player_turn(player_hand, dealer_hand, deck)
-    if bust?(hand_value(player_hand))
-      prompt "You busted!"
-    else
-      prompt "Dealer is thinking..."
-      sleep(1)
-      dealer_turn(dealer_hand, deck)
-      if bust?(hand_value(dealer_hand))
-        prompt "Dealer busted!"
-      end
-    end
-    game_over(player_hand, dealer_hand, false, true)
-    prompt "Play again?"
-    answer = gets.chomp
-    break if answer == 'no'
+def game_over(player_hand, dealer_hand, obscure, dealer_point)
+  game_status(player_hand, dealer_hand, obscure, dealer_point)
+  if determine_winner(player_hand, dealer_hand) == :dealer_win
+    prompt "Dealer wins!"
+  elsif determine_winner(player_hand, dealer_hand) == :player_win
+    prompt "You win!"
+  elsif determine_winner(player_hand, dealer_hand) == :tie
+    prompt "It's a tie!"
   end
 end
 
@@ -171,5 +174,5 @@ dealer = {
 }
 
 prompt "Welcome to Twenty-One!"
-play_game
+play_game(player, dealer)
 prompt "Thank you for playing Twenty-One!"
